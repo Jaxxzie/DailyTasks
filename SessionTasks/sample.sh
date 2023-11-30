a66b0b9d34cefd77eea9494b150ac12ea3575253
Jaxxzie <arunjava001@gmail.com>
2023-11-30 21:27:55 +0530

    Created directory hierarchy

---
+++ SessionTasks/sample.sh
@@ -0,0 +1,19 @@
+
+
+#!/bin/bash
+#Author:Nandan
+#Date:Inikidan
+
+echo "Name:"
+read name
+age="29"
+echo "Hello,$name!"
+
+echo "Age:$age"
+
+echo "System Variables"
+
+echo 'B='$BASH
+echo 'host='$HOSTNAME
+echo 'version='$BASH_VERSION
+
